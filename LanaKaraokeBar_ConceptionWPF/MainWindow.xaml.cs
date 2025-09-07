b7d82c022cfb50e77b624252f140a3c51583a3c2
Denoos <148026464+Denoos@users.noreply.github.com>
2025-09-08 09:13:04 +1000

    Добавьте файлы проекта.

---
+++ LanaKaraokeBar_ConceptionWPF/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using LanaKaraokeBar_ConceptionWPF.Pages;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace LanaKaraokeBar_ConceptionWPF
+{
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public Page CurrentPage { get => currentPage; set { currentPage = value; Signal(); } }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private Page currentPage;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            SetCurrentPage(new WelcomePage());
+        }
+
+        private void Signal(string? prop = null)
+        { 
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        private void SetCurrentPage(Page page)
+        {
+            CurrentPage = page;
+        }
+
+        private void ShowConcept_Click(object sender, RoutedEventArgs e)
+        {
+            SetCurrentPage(new ConceptPage());
+        }
+
+        private void ShowWireFrame_Click(object sender, RoutedEventArgs e)
+        {
+            SetCurrentPage(new WireFramePage());
+        }
+
+        private void ShowStructure_Click(object sender, RoutedEventArgs e)
+        {
+            SetCurrentPage(new StructurePage());
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e) 
+        {
+            Close();
+        }
+    }
+}
