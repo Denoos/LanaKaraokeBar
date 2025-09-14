453bb1e8a649933e087d34d7e8d485ded1ad36f4
Denoos <148026464+Denoos@users.noreply.github.com>
2025-09-14 23:24:49 +1000

    Created directories that demonstrate project`s structure

---
+++ LanaKaraokeBar_TvWpf/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace LanaKaraokeBar_TvWpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
