1112ac6d8b30b6b9d87bc5571ebb8b2c1c7d26f6
Denoos <148026464+Denoos@users.noreply.github.com>
2025-10-09 13:11:26 +1000

    Added endpoint for "User" model

---
+++ LanaKaraokeBar_DataBaseApi/Controllers/EndPoints/UsersController.cs
@@ -0,0 +1,50 @@
+﻿using LanaKaraokeBar_DataBaseApi.Controllers.Routers;
+using LanaKaraokeBar_DataBaseApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanaKaraokeBar_DataBaseApi.Controllers.EndPoints
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        UserRouter _router = new();
+
+        [HttpGet("GetAllUsers")]
+        public List<User> GetUser()
+        {
+            return _router.ComposeGetRoute();
+        }
+
+        [HttpPost("AddUser")]
+        public bool PostUser(User value)
+        {
+            return _router.ComposePostRoute(value);
+        }
+
+        [HttpPut("EditUser")]
+        public bool PutUser(User value)
+        {
+            return _router.ComposePutRoute(value);
+        }
+
+        [HttpPut("BlockUser")]
+        public bool BlockUser(User value)
+        {
+            return _router.ComposeBlockRoute(value);
+        }
+
+        [HttpPut("UnBlockUser")]
+        public bool UnBlockUser(User value)
+        {
+            return _router.ComposeUnBlockRoute(value);
+        }
+
+        [HttpDelete("DeleteUser")]
+        public bool DeleteUser(User value)
+        {
+            return _router.ComposeDeleteRoute(value);
+        }
+    }
+}
---
+++ LanaKaraokeBar_DataBaseApi/Controllers/Routers/UserRouter.cs
@@ -0,0 +1,87 @@
+﻿using LanaKaraokeBar_DataBaseApi.Controllers.Services;
+using LanaKaraokeBar_DataBaseApi.Models;
+
+namespace LanaKaraokeBar_DataBaseApi.Controllers.Routers
+{
+    public class UserRouter
+    {
+
+        #region Fields
+
+        private DbRequstsHandler _requester;
+        private ValidationHandler _validator;
+        private EncryptionHandler _encryption;
+        private bool _invalidValueFlag;
+
+        #endregion
+
+        #region Constructor
+
+        public UserRouter()
+        {
+            _validator = new();
+            _encryption = new();
+            _requester = DbRequstsHandler.Instance;
+            _invalidValueFlag = false;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public List<User> ComposeGetRoute()
+        {
+            return _requester.GetAllUsers();
+        }
+
+        public bool ComposePostRoute(User value)
+        {
+            if (_validator.CheckUserIsValid(value))
+            { 
+                value.Password = _encryption.Encrypt(value.Password);
+
+                return _requester.AddUser(value);
+            }
+
+            return _invalidValueFlag;
+        }
+
+        public bool ComposePutRoute(User value)
+        {
+            if (_validator.CheckUserIsValid(value))
+            {
+                value.Password = _encryption.Encrypt(value.Password);
+
+                return _requester.EditUser(value);
+            }
+
+            return _invalidValueFlag;
+        }
+
+        public bool ComposeDeleteRoute(User value)
+        {
+            if (_validator.CheckUserIsValid(value))
+                return _requester.DeleteUser(value);
+
+            return _invalidValueFlag;
+        }
+
+        public bool ComposeBlockRoute(User value)
+        {
+            if (_validator.CheckUserIsValid(value))
+                return _requester.BlockUser(value);
+
+            return _invalidValueFlag;
+        }
+
+        public bool ComposeUnBlockRoute(User value)
+        {
+            if (_validator.CheckUserIsValid(value))
+                return _requester.UnBlockUser(value);
+
+            return _invalidValueFlag;
+        }
+
+        #endregion
+    }
+}
